0b3f9e5820ce90da7c45891e9c3809d9a18c5013
libakoer <arturtamm02@gmail.com>
2024-11-23 18:32:51 +0200

    parem footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,47 @@
-<script>
-export default {
-  name: "customFooter",
-}
-</script>
-
 <template>
-  <footer>
-    <p>There should be something in the footer ig</p>
+  <footer class="footer">
+    <div class="footer-content">
+      <p>&copy; 2024 Facenook. All rights reserved.</p>
+      <button @click="scrollToTop" class="back-to-top">Back to Top</button>
+    </div>
   </footer>
 </template>
 
+<script>
+export default {
+  name: "customFooter",
+  methods: {
+    scrollToTop() {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    },
+  },
+};
+</script>
+
 <style scoped>
-footer {
-  margin-top: 5px;
+.footer {
   background-color: #696969;
   width: 100%;
+  color: #fff;
+  text-align: center;
+  padding: 3px;
+
+}
+.footer-content a:hover {
+  color: #fff;
 }
 
-footer p{
-  font-size: 1rem;
+.back-to-top {
+  background: none;
+  border: none;
+  color: #bbb;
+  cursor: pointer;
+  margin-top: 10px;
+  font-size: 0.9em;
+  transition: color 0.3s;
+}
+
+.back-to-top:hover {
+  color: #fff;
 }
 </style>
